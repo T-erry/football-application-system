f7913945c49219e1e34991fabf663bf1986cd550
T-erry <terrywachuka10@gmail.com>
2025-03-11 14:56:36 +0300

    customized the user Admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 # Register your models here.
-admin.site.register(User)
+# admin.site.register(User)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['email', 'first_name', 'last_name', "is_staff", 'public_id']
+
+    search_fields = ['email','first_name', 'last_name']
+
+    ordering = ['email']
+    
+
+    fieldsets = (
+        (
+            (None, {"fields": ("password",)})
+        ),
+        (
+
+            ("Personale Info", {"fields": ('first_name', 'last_name', 'email')})
+        ),
+        (
+            ("Permissions", {"fields": ('is_active',  'is_staff', 'is_superuser', 'groups', 'user_permissions')})
+        ),
+        (
+            ("Imporatant Dates", {"fields": ("last_login",)})
+        ),
+        )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide'),
+            'fields': ("first_name","last_name" ,'email', 'password1', 'password2'),
+        }),
+    )
+        
+    
